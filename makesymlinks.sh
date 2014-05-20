349249a304218f052817e40269e6db01f4740892
Josh Newman <josh.newman4390@gmail.com>
2014-05-20 12:24:27 +1000

    Now symlinks gitconfig

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,16 +6,21 @@
 # \curl -L http://install.ohmyz.sh | sh
 # \wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 
-dir=~/.dotfiles        # dotfiles directory
+dir=$HOME/.dotfiles        # dotfiles directory
 
-# link zshrc
-echo "Linking .zshrc to $dir/zshrc..."
-rm $HOME/.zshrc
-ln -s $dir/zshrc ~/.zshrc
-echo "...done"
+dotfiles=(zshrc gitconfig)
+
+for file in ${dotfiles[@]}; do
+  echo "-- Removing existing .${file}"
+  rm "$HOME/.${file}"
+  echo "++ Symlinking .${file}"
+  ln -s $dir/$file "$HOME/.${file}"
+done
+unset file
 
 # link zsh theme
-echo "Linking theme to $HOME/.oh-my-zsh/themes..."
+echo "-- Removing existing joshnewman.zsh-theme"
 rm ~/.oh-my-zsh/themes/joshnewman.zsh-theme
+echo "++ Symlinking joshnewman.zsh-theme"
 ln -s $dir/joshnewman.zsh-theme ~/.oh-my-zsh/themes
 echo "...done"
